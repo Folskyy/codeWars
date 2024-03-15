5096297b8b67b668ec50bf558868b0f15061fdb7
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-03-14 22:24:30 -0300

    first commit

---
+++ split_strings.py
@@ -0,0 +1,7 @@
+def solution(arr):
+    arr2 = []
+    for i in range(0, len(arr), 2): # percorre toda a palavra pulando 2 casas por vez
+        arr2.append(arr[i:i+2]) # adiciona os 2 chars como um novo elemento de uma array
+    if len(arr) % 2 == 1:
+        arr2[-1] += '_' # adiciona um underline caso a string possua tamanho impar
+    return arr2
