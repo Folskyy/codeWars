039ec7eeeb25ef1ece18b3c4ea2f3cfb8673209e
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-03-17 05:22:54 -0300

    replace with alphabet position

--- replace_with_alphabet_position.py
+++ replace_with_alphabet_position.py
@@ -2,5 +2,9 @@ def alphabet_position(var):
     result = []
     for letter in var.lower():
         if 97 <= ord(letter) <= 122:
-            result.append(str(ord(letter) - ord()))
+            result.append(str(ord(letter) - ord("a")))
     return f"{' '.join(result)}"
+
+text = "You are young and life is long!"
+
+alphabet_position(text)
